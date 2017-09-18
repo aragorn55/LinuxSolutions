5de12bfeb43515904e908029690e4cca42d0a6ca
Unknown <aragorn55@gmail.com>
2017-09-18 18:49:50 -0500

    linux setup saves
    reset incase

---
+++ actions/jdisk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'java -jar -Dpath=$1 /home/joshua/bin/jdiskreport-1.4.1/jdiskreport-1.4.1.jar'
+java -jar -Dpath=$1 /home/joshua/bin/jdiskreport-1.4.1/jdiskreport-1.4.1.jar
---
+++ nemo_jdisk_create.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp actions/jdisk.sh ~/.local/share/nemo/actions/jdisk.sh
+cp actions/jdisk.nem ~/.local/share/nemo/actions/jdisk.nemo_action
+chmod a+x -R ~/.local/share/nemo/scripts/*
